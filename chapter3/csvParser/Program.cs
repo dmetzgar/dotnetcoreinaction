9681430b02dd3eb9813c27fc8951a3b5ec4ced2d
dmetzgar <dmetzgar@outlook.com>
2016-08-09 01:12:51 -0700

    Section 3.1. CSV parser sample project

---
+++ chapter3/csvParser/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+       string csv = @"Year,Title,Production Studio
+2008,Iron Man,Marvel Studios
+2008,The Incredible Hulk,Marvel Studios
+2008,Punisher: War Zone,Marvel Studios
+2009,X-Men Origins: Wolverine,20th Century Fox
+2010,Iron Man 2,Marvel Studios
+2011,Thor,Marvel Studios
+2011,X-Men: First Class,20th Century Fox
+";
+      StringReader sr = new StringReader(csv);
+      var csvReader = new CsvReader(sr);
+      foreach (var line in csvReader.Lines)
+        Console.WriteLine(line.First(p => p.Key == "Title").Value);
+    }
+  }
+}
