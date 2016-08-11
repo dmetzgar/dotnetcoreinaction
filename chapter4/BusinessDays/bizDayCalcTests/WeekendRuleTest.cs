ec7d1f343b2d1216afec5ca27c4fe767c2290b04
dmetzgar <dmetzgar@outlook.com>
2016-08-11 01:30:19 -0700

    Modified WeekendRuleTest theories to have multiple parameters

--- chapter4/BusinessDays/bizDayCalcTests/WeekendRuleTest.cs
+++ chapter4/BusinessDays/bizDayCalcTests/WeekendRuleTest.cs
@@ -7,21 +7,14 @@ namespace BusinessDaysTest
   public class WeekendRuleTest
   {
     [Theory]
-    [InlineData("2016-06-27")]
-    [InlineData("2016-03-01")]
-    public void IsBusinessDay(string date)
+    [InlineData(true, "2016-06-27")]
+    [InlineData(true, "2016-03-01")]
+    [InlineData(false, "2016-06-26")]
+    [InlineData(false, "2016-11-12")]
+    public void IsBusinessDay(bool expected, string date)
     {
       var rule = new WeekendRule();
-      Assert.True(rule.CheckDate(DateTime.Parse(date)));
-    }
-
-    [Theory]
-    [InlineData("2016-06-26")]
-    [InlineData("2016-11-12")]
-    public void IsNotBusinessDay(string date)
-    {
-      var rule = new WeekendRule();
-      Assert.False(rule.CheckDate(DateTime.Parse(date)));
+      Assert.Equal(expected, rule.CheckDate(DateTime.Parse(date)));
     }
   }
 }
