8288b39d7c6c754eb4665d269e42d4f3e59840b9
dmetzgar <dmetzgar@outlook.com>
2016-08-11 01:31:55 -0700

    Modified WeekendRuleTest theories to use MemberData

--- chapter4/BusinessDays/bizDayCalcTests/WeekendRuleTest.cs
+++ chapter4/BusinessDays/bizDayCalcTests/WeekendRuleTest.cs
@@ -1,20 +1,27 @@
 using System;
+using System.Collections.Generic;
 using BusinessDays;
 using Xunit;
 
 namespace BusinessDaysTest
 {
-  public class WeekendRuleTest
+  public class WeekendRuleTest2
   {
+    public static IEnumerable<object[]> Days {
+      get {
+        yield return new object[] {true, new DateTime(2016, 6, 27)};
+        yield return new object[] {true, new DateTime(2016, 3, 1)};
+        yield return new object[] {false, new DateTime(2016, 6, 26)};
+        yield return new object[] {false, new DateTime(2016, 11, 12)};
+      }
+    }
+
     [Theory]
-    [InlineData(true, "2016-06-27")]
-    [InlineData(true, "2016-03-01")]
-    [InlineData(false, "2016-06-26")]
-    [InlineData(false, "2016-11-12")]
-    public void IsBusinessDay(bool expected, string date)
+    [MemberData(nameof(Days))]
+    public void TestCheckDate(bool expected, DateTime date)
     {
       var rule = new WeekendRule();
-      Assert.Equal(expected, rule.CheckDate(DateTime.Parse(date)));
+      Assert.Equal(expected, rule.CheckDate(date));
     }
   }
 }
