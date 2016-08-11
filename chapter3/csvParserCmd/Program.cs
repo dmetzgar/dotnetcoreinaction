11eb8c322d8afc8ac889f5a823b4cec89e385a6a
dmetzgar <dmetzgar@outlook.com>
2016-08-11 01:00:44 -0700

    Created csvParserCmd with project dependency on csvParser.

--- chapter3/csvParserCmd/Program.cs
+++ chapter3/csvParserCmd/Program.cs
@@ -9,8 +9,7 @@ namespace ConsoleApplication
   {
     public static void Main(string[] args)
     {
-      var stream = typeof(Program).GetTypeInfo().Assembly.GetManifestResourceStream("Test1.csv");
-      StreamReader sr = new StreamReader(stream);
+      StreamReader sr = new StreamReader(new FileStream("Marvel.csv", FileMode.Open));
       var csvReader = new CsvReader(sr);
       foreach (var line in csvReader.Lines)
         Console.WriteLine(line.First(p => p.Key == "Title").Value);
