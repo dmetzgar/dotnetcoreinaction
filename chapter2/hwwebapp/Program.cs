1c0334eed4cf2493c40a0b4416722f345bb2fd56
dmetzgar <dmetzgar@outlook.com>
2017-08-11 08:21:42 -0700

    chapter 2

---
+++ chapter2/hwwebapp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace hwwebapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:5000/")
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ chapter2/hwwebapp/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace hwwebapp
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            app.Run(async (context) =>
+                await context.Response.WriteAsync("Hello World!")
+            );
+        }
+    }
+}
