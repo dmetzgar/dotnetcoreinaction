7759cc4c665b92c65cb51f719f505332f2c6a676
dmetzgar <dmetzgar@outlook.com>
2017-08-11 09:02:27 -0700

    chapter 3

---
+++ chapter3/CsvParser4/CsvReader.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.IO;
+
+namespace CsvParser
+{
+    public class CsvReader
+    {
+        private string[] columns;
+        private TextReader source;
+
+        public CsvReader(TextReader reader)
+        {
+            this.source = reader;
+            var columnLine = reader.ReadLine();
+            this.columns = columnLine.Split(',');
+        }
+
+        public IEnumerable<KeyValuePair<string, string>[]> Lines
+        {
+            get
+            {
+                string row;
+                while ((row = this.source.ReadLine()) != null)
+                {
+                    var cells = row.Split(',');
+                    var pairs = new KeyValuePair<string, string>[columns.Length];
+                    for (int col = 0; col < columns.Length; col++)
+                    {
+                        pairs[col] = new KeyValuePair<string, string>(columns[col], cells[col]);
+                    }
+
+                    yield return pairs;
+                }
+            }
+        }
+    }
+}
---
+++ chapter3/CsvParser4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace CsvParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stream = typeof(Program).GetTypeInfo().Assembly.GetManifestResourceStream("CsvParser.Marvel.csv");
+            var reader = new StreamReader(stream);
+            var csvReader = new CsvReader(reader);
+            foreach (var line in csvReader.Lines)
+                Console.WriteLine(line.First(p => p.Key == "Title").Value);
+        }
+    }
+}
